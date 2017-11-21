6b231e8c51ee0cda62a909a880cc8843c00b558f
Peter <peter.prentiss@gmail.com>
2017-11-21 14:04:14 -0600

    Errors and styling display conditionally.

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -3,21 +3,33 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className= `form-group ${touched && error ? 'has-danger' : ''}`
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input {...field.input} type="text" className="form-control"/>
-                {field.meta.error}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         )
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field label="Title" name="title" component={this.renderField} />
                 <Field label="Categories" name="categories" component={this.renderField} />
                 <Field label="Post Content" name="content" component={this.renderField} />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         )
     }
