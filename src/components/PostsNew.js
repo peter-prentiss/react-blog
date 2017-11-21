f75b9ee6db025af84887076d4c7a98ad28ed592f
Peter <peter.prentiss@gmail.com>
2017-11-21 14:57:45 -0600

    Redux wired up and form successfully posts.

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -18,7 +21,7 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values);
     }
 
     render() {
@@ -30,6 +33,7 @@ class PostsNew extends Component {
                 <Field label="Categories" name="categories" component={this.renderField} />
                 <Field label="Post Content" name="content" component={this.renderField} />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         )
     }
@@ -54,4 +58,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
