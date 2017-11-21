522552d4daebc024c5734974dbf9d16e45142d94
Peter <peter.prentiss@gmail.com>
2017-11-21 12:28:14 -0600

    Add validation to form.

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -19,13 +19,30 @@ class PostsNew extends Component {
         return (
             <form>
                 <Field label="Title" name="title" component={this.renderField} />
-                <Field label="Tags" name="tags" component={this.renderField} />
+                <Field label="Categories" name="categories" component={this.renderField} />
                 <Field label="Post Content" name="content" component={this.renderField} />
             </form>
         )
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title || values.title.length < 3) {
+        errors.title = "Please enter a title that is at least 3 characters";
+    }
+    if (!values.categories) {
+        errors.title = "Enter some categories";
+    }
+    if (!values.content) {
+        errors.title = "Enter some content, please";
+    }
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
