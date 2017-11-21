1f02de465caf45ff8fe510c410e07a1cfb5093da
Peter <peter.prentiss@gmail.com>
2017-11-21 16:04:38 -0600

    map ownprops state to props

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params.id;
+        this.props.fetchPost(id);
+    }
+    
     render() {
         return (
             <div>
                 PostShow
             </div>
-        )
+        );
     }
 }
 
-export default PostShow;
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(null, { fetchPost })(PostShow);
