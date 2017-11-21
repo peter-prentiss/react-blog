e640ca1801157f9f58214aa146f7f32d35886e07
Peter <peter.prentiss@gmail.com>
2017-11-21 16:13:00 -0600

    PostShow displays correct info

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -4,14 +4,22 @@ import { fetchPost } from '../actions';
 
 class PostShow extends Component {
     componentDidMount() {
-        const { id } = this.props.match.params.id;
+        const id = this.props.match.params.id;
         this.props.fetchPost(id);
     }
     
     render() {
+        const { post } = this.props;
+
+        if(!post) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
-                PostShow
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
@@ -21,4 +29,4 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(null, { fetchPost })(PostShow);
+export default connect(mapStateToProps, { fetchPost })(PostShow);
