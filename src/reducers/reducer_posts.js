2769a93bfe90e1a4845a87b3dffaf46a3a45a37e
Peter <peter.prentiss@gmail.com>
2017-11-21 16:51:36 -0600

    Complete basic blog app.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
 import { FETCH_POST } from '../actions';
+import { DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POST:
             return { ...state, [action.payload.data.id]: action.payload.data };
         case FETCH_POSTS:
